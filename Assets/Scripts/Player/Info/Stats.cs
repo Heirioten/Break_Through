ab4a501dfa26cf3b3ef4a66693140464524cfc44
Nicholas <nicholasjohnlyons@gmail.com>
2025-03-31 23:30:24 -0400

    Fix Laserwall Damage

--- Assets/Scripts/Player/Info/Stats.cs
+++ Assets/Scripts/Player/Info/Stats.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:783032f7f4e16c075ebf3c952f4d54a73408e8833bf699ffa6641f462c863059
-size 674
+oid sha256:06f20a1aab2d2ee70f760d9d74ad61daa54924b401c254ffc1ab48926ac3a23b
+size 688
